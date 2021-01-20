014560993958812992b280ecec401ea2c1764a0d
Anxiao Lu <anxiao.lu@intel.com>
2021-01-20 20:18:28 +0800

    Add cmake option for OpenGL

--- builder/FindOpenGL.cmake
+++ builder/FindOpenGL.cmake
@@ -1,15 +1,15 @@
-# Copyright (c) 2018 Intel Corporation
-#
+# Copyright (c) 2017 Intel Corporation
+# 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-#
+# 
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-#
+# 
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -18,16 +18,22 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-add_subdirectory(sample_common)
-add_subdirectory(sample_decode)
-add_subdirectory(sample_encode)
-add_subdirectory(sample_fei)
-add_subdirectory(sample_hevc_fei)
-add_subdirectory(sample_hevc_fei_abr)
-add_subdirectory(sample_vpp)
-add_subdirectory(sample_multi_transcode)
-add_subdirectory(sample_misc/wayland)
-add_subdirectory(sample_plugins/rotate_cpu)
-add_subdirectory(sample_plugins/rotate_opencl)
-add_subdirectory(sample_plugins/vpp_plugin)
-add_subdirectory(metrics_monitor)
+find_path( OPENGL_INCLUDE GL/gl.h EGL/egl.h EGL/eglext.h gbm.h PATHS /usr/local/include /usr/include)
+find_library( OPENGL_LIBRARY libGL.so libEGL.so libgbm.so PATHS /usr/local/lib64 /usr/lib64)
+
+if (ENABLE_OPENGL)
+    if ( NOT OPENGL_INCLUDE MATCHES NOTFOUND )
+        if ( NOT OPENGL_LIBRARY MATCHES NOTFOUND )
+            set( OPENGL_FOUND TRUE )
+            add_definitions( -DUSE_OPENGL )
+            get_filename_component( OPENGL_LIBRARY_PATH ${OPENGL_LIBRARY} PATH )
+            list( APPEND OPENGL_LIBS OpenGL )
+        endif()
+    endif()
+endif()
+
+if ( NOT DEFINED OPENGL_FOUND )
+  message( STATUS "OpenGL was not found (optional) or not required by the user. The following will not be built: render + encode pipeline.")
+else ()
+  message( STATUS "OpenGL was found here: ${OPENGL_LIBRARY_PATH} and ${OPENGL_INCLUDE}" )
+endif()
