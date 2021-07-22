ea592dc8bd8536c9737b9cb9099f85f6b3d72c05
Wang Dylan <dylan.wang@intel.com>
2021-07-22 13:57:25 +0800

    [HEVCe]Fix unknown FrameType error for FFmpeg(2)
    Signed-off-by: Wang Dylan <dylan.wang@intel.com>

--- _studio/enctools/src/mfx_enctools_common.cpp
+++ _studio/enctools/src/mfx_enctools_common.cpp
@@ -368,6 +368,8 @@ mfxStatus EncTools::InitVPP(mfxEncToolsCtrl const& ctrl)
         sts = m_pmfxVPP_SCD->Init(&m_mfxVppParams_AEnc);
         MFX_CHECK_STS(sts);
 
+        //memory allocation for SCD
+        m_IntSurfaces_SCD = {};
         m_IntSurfaces_SCD.Info = m_mfxVppParams_AEnc.vpp.Out;
         m_IntSurfaces_SCD.Data.Y = new mfxU8[m_IntSurfaces_SCD.Info.Width * m_IntSurfaces_SCD.Info.Height * 3 / 2];
         m_IntSurfaces_SCD.Data.UV = m_IntSurfaces_SCD.Data.Y + m_IntSurfaces_SCD.Info.Width * m_IntSurfaces_SCD.Info.Height;
@@ -467,9 +469,7 @@ mfxStatus EncTools::CloseVPP()
     if (m_IntSurfaces_SCD.Data.Y)
     {
         delete[] m_IntSurfaces_SCD.Data.Y;
-        m_IntSurfaces_SCD.Data.Y = nullptr;
-        m_IntSurfaces_SCD.Data.UV = nullptr;
-        m_IntSurfaces_SCD.Data.Pitch = 0;
+        m_IntSurfaces_SCD = {};
     }
 
     if (m_pmfxVPP_SCD)
